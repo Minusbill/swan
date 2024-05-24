de98d9bc92d62f4a0cb448284d4be219d949f1eb
bill jian <minus502@126.com>
2024-05-24 20:29:43 +0800

    Update Swan.sh

--- Swan.sh
+++ Swan.sh
@@ -4,6 +4,8 @@ read -p "请输入 ECP IP 地址: " ECP_IP
 
 ECP_PORT=9085
 
+read -p "请输入自定义 ECP 节点名称: " node_name
+
 read -p "请输入你的密钥 (不能带 0x 开头): " private_key
 if [[ $private_key == 0x* ]]; then
   echo "密钥不能带 0x 开头"
@@ -30,11 +32,8 @@ wget https://github.com/swanchain/go-computing-provider/releases/download/v0.4.7
 
 chmod +x computing-provider
 
-./computing-provider init --multi-address=/ip4/$ECP_IP/tcp/$ECP_PORT --node-name=swannode
+./computing-provider init --multi-address=/ip4/$ECP_IP/tcp/$ECP_PORT --node-name=$node_name
 
 ./computing-provider wallet import private.key
 
 ./computing-provider account create --ownerAddress $wallet_address --ubi-flag=true
-
-nohup ./computing-provider ubi daemon >> cp.log 2>&1 &
-
